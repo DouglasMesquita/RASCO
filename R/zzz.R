2654706e8be47d80de4965e05fec113ba14cac9a
douglas <douglasrm.azevedo@gmail.com>
2020-02-11 17:41:30 -0300

    Addin INLA workaround in .onLoad just if release == "xenial"

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,13 @@
 .onLoad <- function(libname, pkgname) {
-  # INLA:::inla.dynload.workaround()
+  if(Sys.info()['sysname'] == "Linux") {
+    release <- system(command = "lsb_release -a", intern = TRUE, ignore.stderr = TRUE)
+    release <- paste(gsub(x = release, pattern = "\t", replacement = " "), collapse = " ")
+
+    ubuntu_version <- gsub(x = release, pattern = ".*Codename: ([a-zA-Z]*)$", replacement = "\\1")
+
+    if(ubuntu_version == "xenial") {
+      INLA:::inla.dynload.workaround()
+    }
+  }
   packageStartupMessage("RASCO: version 0.0.3 \n")
 }
