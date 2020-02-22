60c620f7a96225f32286bb54d12fd1281905e990
douglas <douglasrm.azevedo@gmail.com>
2020-02-22 20:58:35 -0300

    Adding slovenia dataset

---
+++ R/slovenia.R
@@ -0,0 +1,16 @@
+#' @title Slovenia stomach cancer dataset
+#'
+#' @description Slovenia stomach cancer dataset
+#'
+#' @docType data
+#'
+#' @usage data(slovenia)
+#'
+#' @format An object of class \code{"sf"}.
+#'
+#' @keywords datasets
+#'
+#' @references Zadnik, Vesna, and B. J. Reich. "Analysis of the relationship between socioeconomic factors and stomach cancer incidence in Slovenia." Neoplasma 53.2 (2006): 103-110.
+#'
+
+"slovenia"
