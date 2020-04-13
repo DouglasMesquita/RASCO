3cb25a88c90e24075111028405ffcad961ada684
douglas <douglasrm.azevedo@gmail.com>
2020-04-13 11:00:53 -0300

    Removing sn and adding stats:: before mean and sd

--- R/chain_summary.R
+++ R/chain_summary.R
@@ -17,8 +17,8 @@
 chain_summary <- function(sample, level = 0.95) {
   if(!is.null(sample)) {
     means <- colMeans(sample)
-    medians <- apply(X = sample, MARGIN = 2, FUN = median)
-    sds <- apply(X = sample, MARGIN = 2, FUN = sd)
+    medians <- apply(X = sample, MARGIN = 2, FUN = stats::median)
+    sds <- apply(X = sample, MARGIN = 2, FUN = stats::sd)
     hpds <- t(apply(X = sample, MARGIN = 2, FUN = function(x) coda::HPDinterval(coda::as.mcmc(x), prob = level)))
 
     out <- cbind.data.frame(round(means, 6), round(medians, 6), round(sds, 6), round(hpds, 6))
